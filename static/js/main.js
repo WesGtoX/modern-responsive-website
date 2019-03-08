857f14c0859922c300698ac7605fc4eb1ce5f575
Wesley Mendes <wesley_gto_x@yahoo.com.br>
2019-03-08 01:29:12 -0300

    update media queries responsive

--- static/js/main.js
+++ static/js/main.js
@@ -33,6 +33,20 @@ $(function() {
         smartSpeed: 700,
         loop: true,
         autoplayHoverPause: true,
+        responsive : {
+            // breakpoint from 0 up
+            0 : {
+                items: 1
+            },
+            // breakpoint from 480 up
+            480 : {
+                items: 2
+            },
+            // breakpoint from 768 up
+            768 : {
+                items: 3
+            },
+        }
     });
 });
 
